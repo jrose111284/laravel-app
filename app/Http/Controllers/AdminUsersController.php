c48bb8199ae555fc5de3b26ce34e384738e82e0e
jamie rose <jrose111284@gmail.com>
2016-11-13 17:51:14 +0000

    added the roles to the form

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Role;
 use App\Http\Requests;
 
 
@@ -30,7 +31,10 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.user.create');
+
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.user.create', compact('roles'));
     }
 
     /**
