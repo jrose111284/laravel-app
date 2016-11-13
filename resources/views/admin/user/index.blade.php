03ab8f7a05b69db906fb42977f5a924cd9959bf1
jamie rose <jrose111284@gmail.com>
2016-11-13 15:17:56 +0000

    yeild the the user create blade file, also added to extra tables to the admin page to see if you are the admin and what role id you have

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -9,6 +9,8 @@
                <th>id</th>
                <th>name</th>
                <th>Email</th>
+               <th>Role</th>
+               <th>Active</th>
                <th>created</th>
                <th>updated</th>
            </tr>
@@ -19,9 +21,11 @@
 
              @foreach($user as $users)
                <tr>
-                 <td>{{$users->id}}</td>
-                 <td>{{$users->name}}</td>
-                 <td>{{$users->email}}</td>
+                   <td>{{$users->id}}</td>
+                   <td>{{$users->name}}</td>
+                   <td>{{$users->email}}</td>
+                   <td>{{$users->role->name}}</td>
+                   <td>{{$users->is_active == 1 ? 'Active' : 'Not Active'}}</td>
                    <td>{{$users->created_at->diffForHumans()}}</td>
                    <td>{{$users->updated_at->diffForHumans()}}</td>
                </tr>
